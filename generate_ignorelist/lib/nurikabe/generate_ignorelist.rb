f5889196aa8df63a78c1195be51f38e9d947c041
epaew <epaew.333@gmail.com>
2020-11-27 08:38:00 +0900

    nurikabe/generate_ignorelist: ほぼ何もしない実行ファイルを追加

---
+++ generate_ignorelist/lib/nurikabe/generate_ignorelist.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require_relative 'generate_ignorelist/command'
+require_relative 'generate_ignorelist/version'
---
+++ generate_ignorelist/lib/nurikabe/generate_ignorelist/command.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Nurikabe
+  module GenerateIgnorelist
+    class Command
+      def initialize(args)
+        pp args
+      end
+
+      def call
+        'called'
+      end
+    end
+  end
+end
