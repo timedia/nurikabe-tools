14fdf0b7805f77d85fc816158eb9144991ecb49a
epaew <epaew.333@gmail.com>
2020-11-27 08:55:24 +0900

    OptionParser の追加

--- generate_ignorelist/lib/nurikabe/generate_ignorelist/command.rb
+++ generate_ignorelist/lib/nurikabe/generate_ignorelist/command.rb
@@ -1,14 +1,33 @@
 # frozen_string_literal: true
 
+require 'optparse'
+require 'yaml'
+
+require_relative 'version'
+
 module Nurikabe
   module GenerateIgnorelist
     class Command
       def initialize(args)
-        pp args
+        # set default values
+        @path = File.expand_path('../../../config/default.yaml', __dir__)
+        @separator = ','
+
+        option_parser.parse(args)
       end
 
       def call
-        'called'
+        [@path, @separator]
+      end
+
+      private
+
+      def option_parser
+        @option_parser ||= OptionParser.new do |opts|
+          opts.on('-f', '--file=FILE') { |path| @path = path }
+          opts.on('-s', '--separator=SEP') { |separator| @separator = separator }
+          opts.version = VERSION
+        end
       end
     end
   end
